53cdcf7b61cc975e187b5e0730e9489de7879397
mikhail-volkov <mikhail-volkov83@yandex.ru>
2020-08-23 16:09:31 +0300

    Создание и вызов функции diff

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,10 @@ function sum(...arg) {
 
     return summ
 }
+
+function diff(...arg) {
+    const dif = arg.reduce((acc, item) => {
+        return acc + -item
+    }, 0)
+    return dif
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 console.log(sum(1, 2, 3))
 console.log(sum(10, 20, 40))
+console.log(diff(10, 20, 40))
